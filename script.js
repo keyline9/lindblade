044ac231201a736193b02c964fbda627b11e0ee0
keyline9 <jayson.allen@gmail.com>
2026-01-07 19:17:14 -0800

    Replaced script.js

--- script.js
+++ script.js
@@ -7,20 +7,24 @@ if (year) year.textContent = new Date().getFullYear();
 if (menuBtn && mobileNav) {
   menuBtn.addEventListener('click', () => {
     const isHidden = mobileNav.hasAttribute('hidden');
+
     if (isHidden) {
       mobileNav.removeAttribute('hidden');
       menuBtn.setAttribute('aria-expanded', 'true');
+      menuBtn.classList.add('is-open');
     } else {
       mobileNav.setAttribute('hidden', '');
       menuBtn.setAttribute('aria-expanded', 'false');
+      menuBtn.classList.remove('is-open');
     }
   });
 
-  // Close mobile nav when clicking a link
-  mobileNav.querySelectorAll('a').forEach(a => {
+  // Close the mobile menu after clicking a link
+  mobileNav.querySelectorAll('a').forEach((a) => {
     a.addEventListener('click', () => {
       mobileNav.setAttribute('hidden', '');
       menuBtn.setAttribute('aria-expanded', 'false');
+      menuBtn.classList.remove('is-open');
     });
   });
 }
